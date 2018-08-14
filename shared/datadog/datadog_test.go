db5531cfc54068c299b8c98e323a1cb5ee27b7ee
Srividya Viveganandan <srividya@nextdoor.com>
2018-08-14 11:19:07 -0700

    Better unittesting for dd

--- shared/datadog/datadog_test.go
+++ shared/datadog/datadog_test.go
@@ -1,12 +1,23 @@
 package datadog
 
 import (
+	"net"
+	"os"
 	"testing"
 
+	"github.com/DataDog/datadog-go/statsd"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLog(t *testing.T) {
+	addr, _ := net.ResolveUDPAddr("udp", "localhost:8125")
+	sock, _ := net.ListenUDP("udp", addr)
+	defer func() {
+		buf := make([]byte, 1024)
+		rlen, _, _ := sock.ReadFromUDP(buf)
+		assert.Contains(t, string(buf[0:rlen]), "conductor testing")
+	}()
+	os.Setenv("STATSD_HOST", "localhost")
 	assert.NotNil(t, c)
-	log("%s testing", "conductor")
+	log(statsd.Info, "%s testing", "conductor")
 }
