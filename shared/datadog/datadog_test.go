61ec6d57278f57dbac1d96ccab0d31db4d932ba8
Srividya Viveganandan <srividya@nextdoor.com>
2018-08-15 16:05:19 -0700

    Remove unnecessary defer

--- shared/datadog/datadog_test.go
+++ shared/datadog/datadog_test.go
@@ -13,23 +13,19 @@ var addr, _ = net.ResolveUDPAddr("udp", "localhost:8125")
 var sock, _ = net.ListenUDP("udp", addr)
 
 func TestLogInfo(t *testing.T) {
-	defer func() {
-		buf := make([]byte, 1024)
-		rlen, _, _ := sock.ReadFromUDP(buf)
-		assert.Contains(t, string(buf[0:rlen]), "conductor testing")
-	}()
 	os.Setenv("STATSD_HOST", "localhost")
 	assert.NotNil(t, c)
 	log(statsd.Info, "%s testing", "conductor")
+	buf := make([]byte, 1024)
+	rlen, _, _ := sock.ReadFromUDP(buf)
+	assert.Contains(t, string(buf[0:rlen]), "conductor testing")
 }
 
 func TestLogError(t *testing.T) {
-	defer func() {
-		buf := make([]byte, 1024)
-		rlen, _, _ := sock.ReadFromUDP(buf)
-		assert.Contains(t, string(buf[0:rlen]), "conductor testing")
-	}()
 	os.Setenv("STATSD_HOST", "localhost")
 	assert.NotNil(t, c)
 	log(statsd.Error, "%s testing", "conductor")
+	buf := make([]byte, 1024)
+	rlen, _, _ := sock.ReadFromUDP(buf)
+	assert.Contains(t, string(buf[0:rlen]), "conductor testing")
 }
