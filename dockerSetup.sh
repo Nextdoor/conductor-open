ee674332dc636ac51f0592af2b8b69a850723e77
Esha Verma <ishita_verma@live.com>
2019-09-17 14:43:42 -0700

    adding few more commands

--- dockerSetup.sh
+++ dockerSetup.sh
@@ -2,14 +2,16 @@
 # Hence you should have docker app downloaded and running on your machine before running this script.
 # It is also adviced to increase available memory for the docker app more than the default setting, to speed up performance.
 
-
 #!/bin/bash
 
 PINK='\033[0;35m'
 NC='\033[0m'        # No Color
 
-# echo -e "${PINK}checking install of package management tools..${NC}"
+echo -e "${PINK}checking install of package management tools..${NC}"
 if ! brew ls --versions yarn; then brew install yarn; fi; 
+
+echo -e "${PINK}stop all existing containers to avoid attached port conflicts..${NC}"
+docker container stop $(docker container ls -aq)
 
 echo -e "${PINK}bringing up new postgres docker container for conductor...${NC}"
 make postgres
@@ -23,9 +25,11 @@ make test-data
 echo -e "${PINK}building react.js and frontend static files webpack into resources/frontend...${NC}"
 make prod-compile -C frontend
 
+echo -e "${PINK} Stop potential running ngnix (from nativeMacSetup) to avoid port conflict...${NC}"
+sudo nginx -s stop
+
 echo -e "${PINK}building backend conductor service${NC}"
 make docker-build
 
 echo -e "${PINK}starting conductor service${NC}"
 make docker-run
-
