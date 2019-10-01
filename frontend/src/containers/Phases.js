0c5a3fd14d525fdfece62246ea6519b15527bd3c
Esha Verma <ishita_verma@live.com>
2019-09-30 17:27:27 -0700

    Update Phases.js

--- frontend/src/containers/Phases.js
+++ frontend/src/containers/Phases.js
@@ -1,14 +1,28 @@
 import {connect} from 'react-redux';
 
+import Actions from 'actions';
 import Component from 'components/Phases';
 
 const mapStateToProps = (state) => {
   return {
+    self : state.self,
     train: state.train.details,
-    request: state.train.request
+    request: state.train.request,
+    requestRestart: state.train.requestRestart,
   };
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    restartJob: (trainId, phaseName) => {
+      dispatch(Actions.Train.restart(trainId, phaseName));
+    }
+  };
+};
+
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Component);
+
