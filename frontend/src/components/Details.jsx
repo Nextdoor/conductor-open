aa802554c91c8153883ddc0746fdc42980bfbaca
Esha Verma <ishita_verma@live.com>
2019-10-02 13:58:44 -0700

    Update Details.jsx - more editing needed

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import moment from 'moment';
 
+import ApiButton from 'components/ApiButton';
 import Card from 'components/Card';
 import TrainComponent from 'components/TrainComponent';
 import TitledList from 'components/TitledList';
 import {trainProps, requestProps} from 'types/proptypes';
 
 class Details extends TrainComponent {
+
   render() {
     return (
       <Card className="details-card" header="Details">
         {this.getComponent()}
+        {this.claimEngineerButton()}
       </Card>
     );
   }
@@ -37,6 +40,29 @@ class Details extends TrainComponent {
     ];
 
     return <TitledList items={items}/>;
+  }
+
+  claimEngineerButton() {
+    return (
+      <ApiButton
+        modalProps={{
+          title: 'Confirm Claiming Engineer Role',
+          body: (
+            <div>
+              Thanks for volunteering to guide this train.
+              <br/><br/>
+              As train engineer, that all commits have been verified (if needed), and that the build together passes, so that the train can proceed to deploy.
+              <br/><br/>
+              Also please toubleshoot (or reach an admin), to take the train to succesful train deployment and closing.
+            </div>
+          )
+        }}
+        //onClick={() => this.props.restartJob(this.props.train.id, 'deploy')}
+        request={this.props.request}
+        className="button-claim">
+        Claim Engineer Role
+      </ApiButton>
+    );
   }
 }
 
