15b1c4493a232537caa3e5ee7151f98d22618c95
Esha Verma <ishita_verma@live.com>
2019-10-03 16:25:25 -0700

    proper babel upgrade (#42)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -83,7 +83,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /(node_modules)/,
         query: {
-          presets: ['es2015', 'react']
+          presets: ['@babel/react', '@babel/env']
         },
         progress: true
       },
